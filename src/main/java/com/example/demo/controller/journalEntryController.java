dfd4d80fb3fb16c6a03d6067f6237a0b26e86b60
Subarna Dahal <subarnadahal456@gmail.com>
2026-02-09 18:39:11 +0545

    did restApi simple application

--- src/main/java/com/example/demo/controller/journalEntryController.java
+++ src/main/java/com/example/demo/controller/journalEntryController.java
@@ -1,23 +1,56 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.journalEntry;
+import com.example.demo.services.journalEntryServices;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/journal")
 public class journalEntryController {
-    public Map<Long,journalEntry> journalEntries=new HashMap<>();
-    @GetMapping("/get-data")
-    public List<journalEntry> getAll(){
-        return  new ArrayList<>(journalEntries.values());
+
+    @Autowired
+    private journalEntryServices journalEntryServices;
+
+    @GetMapping()
+    public List<journalEntry> getAll() {
+        return journalEntryServices.getAll();
     }
-    @PostMapping("/entre-data")
-    public  void createEntry(@RequestBody journalEntry myentry){
-            journalEntries.put(myentry.getId(),myentry);
+
+    @PostMapping()
+    public boolean createEntry(@RequestBody journalEntry myentry) {
+        myentry.setDate(LocalDateTime.now());
+        journalEntryServices.saveEntry(myentry);
+        return true;
+
     }
+
+    @GetMapping("id/{id}")
+    public journalEntry getbyId(@PathVariable ObjectId id) {
+        return journalEntryServices.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("id/{id}")
+    public boolean deleteJournalbyId(@PathVariable ObjectId id) {
+        journalEntryServices.deleteById(id);
+        return true;
+    }
+
+    @PutMapping("id/{id}")
+    public journalEntry updatebyId(@PathVariable ObjectId id, @RequestBody journalEntry myEntry) {
+        journalEntry oldjournalEntry = journalEntryServices.findById(id).orElse(null);
+        if (oldjournalEntry != null) {
+            oldjournalEntry.setTitle(myEntry.getTitle() != null && !myEntry.getTitle().equals("") ? myEntry.getTitle() : oldjournalEntry.getTitle());
+            oldjournalEntry.setContent(myEntry.getContent() != null && !myEntry.getContent().equals("") ? myEntry.getContent() : oldjournalEntry.getContent());
+        }
+        journalEntryServices.saveEntry(oldjournalEntry);
+        return oldjournalEntry;
+    }
+
 }
